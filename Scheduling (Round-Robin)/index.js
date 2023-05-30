cca78e666d4bc916a8d10557de149d6a4c498c57
hirwaaldo1 <hirwaaldo1@gmail.com>
2023-05-30 17:51:24 +0300

    fix: return frist

--- Scheduling (Round-Robin)/index.js
+++ Scheduling (Round-Robin)/index.js
@@ -4,15 +4,16 @@ function roundRobin(jobs, slice, index) {
   while (jobs[index] > 0) {
     for (let i = 0; i < jobs.length; i++) {
       if (jobs[i] !== 0) {
+        if (jobs[index] <= 0) {
+          break;
+        }
+        
         // we must add the slice or if the slice if less that the current job's we add the current job
         const timeTaken = Math.min(jobs[i], slice);
         timeTakes += timeTaken;
         // Subtract the time taken from the current job
         jobs[i] -= timeTaken;
 
-        if (jobs[index] <= 0) {
-          break;
-        }
       }
     }
   }
