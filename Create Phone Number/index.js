adff0ff68d9e525e4ba880a1df1061178073fd35
hirwaaldo1 <hirwaaldo1@gmail.com>
2023-05-31 18:16:34 +0300

    fix: unnessary varibales

--- Create Phone Number/index.js
+++ Create Phone Number/index.js
@@ -1,11 +1,5 @@
-function toString(numbers) {
-  return numbers.join("");
-}
-
 function createPhoneNumber(numbers) {
-  const areaCode = toString(numbers.slice(0, 3));
-  const prefix = toString(numbers.slice(3, 6));
-  const lineNumber = toString(numbers.slice(6));
-
-  return `(${areaCode}) ${prefix}-${lineNumber}`;
+  return `(${numbers.slice(0, 3).join("")}) ${numbers
+    .slice(3, 6)
+    .join("")}-${numbers.slice(6).join("")}`;
 }
